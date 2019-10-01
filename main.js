9763035f3ff02fea2d8c10fe33f6080ab39bf0ee
Célia Chazel <chazel.celia@gmail.com>
2019-10-01 10:02:15 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@
 - Permettre à l'utilisateur de supprimer une tâche
 - Permettre à l'utilisateur de marquer une tâche comme faite/à faire (dans les deux sens)
 - Permettre à l'utilisateur de trier les tâches faites et à faire
-- Permettre à l'utilisateur de rechercher une tâche par chaîne de caractèresxs
+- Permettre à l'utilisateur de rechercher une tâche par chaîne de caractères
 - Bonus : ajouter du LocalStorage pour conserver les données dans le navigateur.
 */
