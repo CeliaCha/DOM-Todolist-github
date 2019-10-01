a8cd85ae7def1730296df305fc0d7d2ba9029199
Célia Chazel <chazel.celia@gmail.com>
2019-10-01 11:24:38 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/* @TODO :
+- Permettre à l'utilisateur d'ajouter une tâche
+- Permettre à l'utilisateur de supprimer une tâche
+- Permettre à l'utilisateur de marquer une tâche comme faite/à faire (dans les deux sens)
+- Permettre à l'utilisateur de trier les tâches faites et à faire
+- Permettre à l'utilisateur de rechercher une tâche par chaîne de caractèresxs
+- Bonus : ajouter du LocalStorage pour conserver les données dans le navigateur.
+*/
+
+console.log('sfsf')
