55e81634818bb72eca56db83a47d7daeadd7e787
Rodrigue <queraudrodrigue@outlook.fr>
2019-10-02 10:54:19 +0200

    add and display task

--- main.js
+++ main.js
@@ -6,3 +6,30 @@
 - Permettre à l'utilisateur de rechercher une tâche par chaîne de caractères
 - Bonus : ajouter du LocalStorage pour conserver les données dans le navigateur.
 */
+const taskname = document.getElementById('taskName')
+const addTaskButton = document.getElementById('addTaskButton')
+const tasklist = document.getElementById('tasklist')
+const tasks = []
+
+const addTodo = () => {
+    const inputValue = taskname.value
+    tasks.push(inputValue)
+
+}
+
+const printTodos = () => {
+    tasklist.innerHTML = ''
+    tasks.forEach(task => {
+        const li = document.createElement('li')
+        li.innerHTML = task
+        tasklist.appendChild(li)
+        taskname.value = ''
+    })
+}
+
+
+addTaskButton.addEventListener('click', () => {
+    addTodo()
+    printTodos()
+    
+})
